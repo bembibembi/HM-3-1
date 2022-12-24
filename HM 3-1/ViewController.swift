094e0d95e8f38aad8c160b50dae7d6cc111a83c7
bembi <ismanovvvabembi123>
2022-12-24 13:15:10 +0600

    Initial Commit

---
+++ HM 3-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM 3-1
+//
+//  Created by 지구9 on 24/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
